cfd184bd6ed26a32a07edcd718fc71ea22bc45a7
Romain666667 <manmanlon90@gmail.com>
2025-09-19 13:57:29 +0200

    Add files via upload

---
+++ TP10/CasserCodePin/CasserCodePin/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace CasserCodePIN
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // start représente l'heure de démarrage du programme
+            DateTime start = DateTime.Now;
+            Console.WriteLine("Début du programme");
+            Random rnd = new Random();
+            int codeSecret = rnd.Next(9999);
+            bool trouve = false;
+            // compteur de boucle
+            int i = 0;
+            while (i < 10000 && trouve == false)
+            {
+                Console.Write(i + " ");
+                // si on trouve le code PIN, il faut afficher i et interrompre la boucle
+                // A COMPLETER...
+                if (i == codeSecret)
+                {
+                    trouve = true;
+                    Console.WriteLine("Le code bon est :" +codeSecret);
+                }
+                Thread.Sleep(5);
+                i++;
+            }   
+            Console.WriteLine("Fin du programme");
+            // durée du programme = heure de fin - heure de démarrage
+            TimeSpan duree = DateTime.Now - start;
+            Console.WriteLine("durée du programme : " + duree);
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
